bef3851bc6467d89642bb319ea49b62dfe8c776b
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-07-24 13:44:20 +0200

    Exercise 1 correction

--- exercice1.sql
+++ exercice1.sql
@@ -1,3 +1,6 @@
 Select articles.id , users.*
 FROM articles
 JOIN users WHERE articles.id_user = users.id ;
+
+// The WHERE clause can be used with cartesian products, for JOIN it's ON
+// The article ID was expected to be 10
